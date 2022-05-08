27683323f5bca8bf15830018fbf644fd7bcca955
Atulit519 <77721130+Atulit519@users.noreply.github.com>
2022-05-08 11:38:23 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,4 @@
+var player_1_username = localStorage.getItem("Player_1");
+var player_2_username = localStorage.getItem("Player_2");
+document.getElementById("player_1_label").innerHTML = player_1_username + " : ";
+document.getElementById("player_2_label").innerHTML = player_2_username + " : ";
